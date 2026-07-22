4924c9e94410b09895ed0c7af9f2423c8d0034ed
anjalisatdeve@gmail.com <anjalisatdeve@gmail.com>
2026-07-22 14:40:37 +0530

    commit

---
+++ 5.Tuple/tuple.py
@@ -0,0 +1,12 @@
+a=(1,2,3,4,5)
+index=a.index(2)
+print(f"Index of 2 in the tuple is: {index}")
+
+b=(1,2,3,8,2,6,7,2)
+count=b.count(2)
+print(f"Count of 2 in the tuple is: {count}")
+
+c,d,e=(1,2,3)
+print(c)
+print(d)
+print(e)
